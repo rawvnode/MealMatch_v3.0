a6f9f507244e0c27c648bfbf95bad13cb7ee55eb
fape5009 <fabianpersson@hotmail.se>
2017-04-27 10:17:14 +0200

    new profile model

--- MealMatch/account_functions/models.py
+++ MealMatch/account_functions/models.py
@@ -4,21 +4,17 @@ import datetime
 from django.db import models
 
 # Create your models here.
-class comments(Document):
-    recipe_id = StringField(required=True)
-    user_comment = StringField(required=True)
+#class comments(Document):
+ #   recipe_id = StringField(required=True)
+  #  user_comment = StringField(required=True)
 
-class User(DynamicDocument):
-    username = StringField(required=True)
-    password = StringField(required=True)
+class Profile(DynamicDocument):
+    id = ObjectIdField(primary_key=True)
     mail = EmailField(required=True)
-    is_staff = BooleanField(default=False)
-    is_superuser = BooleanField(default=False)
-    last_login = DateTimeField(default = datetime.datetime.now)
     favorites = ListField()
     my_recipies = ListField()
-    comments = ListField(EmbeddedDocumentField('comments'))
-    picture = StringField()
+    #comments = ListField(EmbeddedDocumentField('comments'))
+    picture = StringField() #facebook link
     my_info = StringField()
     sex = StringField()
     Pantry = ListField(default= ['Salt', 'Sea salt', 'Pepper','White pepper', 'Vinegar', 'Flour', 'Oil', 'Sugar', 'Pasta', 'Baking powder', 'Soy sauce', 'Broth', 'Honey', 'Tomato purée', 'Cinnamon', 'Oregano', 'Curry'])
